95c10a854c1ceec0dfe1ab741b9a61ff47151989
tay671 <tjone@mymail.mines.edu>
2019-11-21 19:12:56 -0700

    Second Function added

--- Week12_utility.py
+++ Week12_utility.py
@@ -5,3 +5,12 @@
 
 def PrintOutput(a):
     print('OUTPUT', a)
+
+def LoadFile(filename):
+    List = []
+    sublist = []
+    file = open(filename, 'r')
+    List = (file.readlines())
+    for i in List:
+        sublist.append(i[0:-1])
+    return sublist
